62450a38e711017ffae3168218171b1be75bc69b
Jawhar Mohamed <jawhar.med@gmail.com>
2024-08-24 18:24:49 +0200

    first commit

---
+++ src/main/java/org/jm/email/sender/JmEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package org.jm.email.sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmEmailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmEmailSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jm/email/sender/JmEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jm.email.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmEmailSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
